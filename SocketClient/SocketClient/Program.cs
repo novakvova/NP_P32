1905508e7a7d63101a5bd692eb25e6106970be27
novakvova <novakvova@gmail>
2025-05-15 17:28:51 +0300

    Is Good working server

--- SocketClient/SocketClient/Program.cs
+++ SocketClient/SocketClient/Program.cs
@@ -3,9 +3,10 @@ using System.Text;
 
 Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-client.Connect("127.0.0.1", 2318);
+//client.Connect("127.0.0.1", 2318);
+client.Connect("91.238.103.107", 2318);
 
-string message = "Привіт, мій хост!";
+string message = "Привіт, мій хост! То я Вова :)";
 
 client.Send(Encoding.UTF8.GetBytes(message));
 

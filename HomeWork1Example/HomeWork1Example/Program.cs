3c775e3a3d5b4d19e6e874aed747bc08f6df37f0
novakvova <novakvova@gmail>
2025-05-20 15:24:16 +0300

    Add HomeWork1 Example

---
+++ HomeWork1Example/HomeWork1Example/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Text.Json;
+
+class Program
+{
+    static async Task Main()
+    {
+        string folder = @"D:\girl";
+        string url = "https://myp22.itstep.click/api/Galleries/upload";
+
+        if (!Directory.Exists(folder))
+        {
+            Console.WriteLine("Папка не знайдена");
+            return;
+        }
+
+        var imageFiles = Directory.GetFiles(folder, "*.jpg");
+        int images = imageFiles.Length;
+        int count = 0;
+        int secondcount = 0;
+
+        foreach (var imagePath in imageFiles)
+        {
+            if (File.Exists(imagePath))
+            {
+                try
+                {
+                    byte[] bytes = File.ReadAllBytes(imagePath);
+                    string imageBase64 = Convert.ToBase64String(bytes);
+                    //string base64 = $"data:image/png;base64,{imageBase64}";
+
+                    var json = JsonSerializer.Serialize(new { Photo = imageBase64 });
+
+                    using var client = new HttpClient();
+                    //body - запиту
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    //отримує результат
+                    var response = await client.PostAsync(url, content);
+                    //декодуємо результат сервера - читаємо тіло відповіді
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    //Перевіряємо статус відповіді - якщо 200 - то усе супер.  - Сервер фото зберіг
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Програма успішно виконалась: {Path.GetFileName(imagePath)}");
+                        count++;
+                    }
+                    else //Щось пішло не так
+                    {
+                        Console.WriteLine($"Виникла помилка: {Path.GetFileName(imagePath)} - {result}");
+                        secondcount++;
+                    }
+                }
+                catch (Exception ex) //якась глобальна помилка - наприклад сервер потух, місце кончилося
+                {
+                    Console.WriteLine($"Помилка при обробці файлу: {ex.Message}");
+                    secondcount++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Успішно надіслано: {count} з {images}");
+        Console.WriteLine($"Кількість не надісланих файлів: {secondcount}");
+    }
+}
---
+++ ServerAsyncSocket/ServerAsyncSocket/AsyncTcpServer.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ServerAsyncSocket;
+
+// Клас асинхронного TCP-сервера
+class AsyncTcpServer
+{
+    // Статичний метод для запуску сервера
+    public static async Task StartServerAsync()
+    {
+        // Створюємо сокет, який використовує IPv4, потоковий тип (TCP), і протокол TCP
+        var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Прив'язуємо сокет до всіх доступних IP-адрес машини і порту 8080
+        listener.Bind(new IPEndPoint(IPAddress.Any, 1345));
+
+        // Починаємо слухати підключення з чергою на 100 клієнтів
+        listener.Listen(100);
+
+        Console.WriteLine("Сервер слухає на порту 1345...");
+
+        // Нескінченний цикл: постійно очікуємо нові підключення
+        while (true)
+        {
+            // Асинхронно приймаємо підключення клієнта (не блокує потік)
+            Socket clientSocket = await listener.AcceptAsync();
+
+            // Обробка клієнта запускається у фоновому режимі (без очікування завершення)
+            _ = HandleClientAsync(clientSocket);
+
+        }
+        
+    }
+
+    // Метод для обробки кожного окремого клієнта
+    private static async Task HandleClientAsync(Socket clientSocket)
+    {
+        Console.WriteLine("Клієнт підключився.");
+
+        // Виділяємо буфер для прийому даних (до 1024 байт)
+        byte[] buffer = new byte[1024];
+
+        // Асинхронно отримуємо дані від клієнта
+        int received = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+
+        // Перетворюємо отримані байти в текст за допомогою UTF8-кодування
+        string request = Encoding.UTF8.GetString(buffer, 0, received);
+        Console.WriteLine($"Запит: {request}");
+
+        // Формуємо відповідь у вигляді тексту
+        string response = "Привіт від сервера!";
+        // Перетворюємо відповідь у байти
+        byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+        // Асинхронно надсилаємо відповідь клієнту
+        await clientSocket.SendAsync(responseBytes, SocketFlags.None);
+
+        // Завершуємо з'єднання (з обох сторін: надсилання і прийом)
+        clientSocket.Shutdown(SocketShutdown.Both);
+
+        // Закриваємо сокет
+        clientSocket.Close();
+    }
+}

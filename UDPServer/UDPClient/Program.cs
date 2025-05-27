8323afc3cbe5cd1fe432ad14e761510bb138f0f9
novakvova <novakvova@gmail.com>
2025-05-27 15:38:07 +0300

    Udp is good

---
+++ UDPServer/UDPClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8; // Встановлюємо кодування для консолі
+Console.OutputEncoding = Encoding.UTF8; // Встановлюємо кодування для виводу в консоль
+
+UdpClient udpClient = new UdpClient();
+
+Console.WriteLine("Вкажіть повідомлення:");
+string message  = Console.ReadLine();
+var bytes = Encoding.UTF8.GetBytes(message);
+string ip = "127.0.0.1";
+int port = 1028;
+
+await udpClient.SendAsync(bytes, ip, port);
+
+Console.WriteLine("Повідомлення надіслано! :)");
---
+++ UDPServer/UDPServer/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8; // Встановлюємо кодування для консолі
+Console.OutputEncoding = Encoding.UTF8; // Встановлюємо кодування для виводу в консоль
+
+var udpClient = new UdpClient(1028); //0 - 65535 - 2 байти
+Console.WriteLine("UDP Server is running on port 1028...");
+
+while(true)
+{
+    //Очікуємо повідомлення від клієнта
+    var result = await udpClient.ReceiveAsync();
+    string text = Encoding.UTF8.GetString(result.Buffer); // отримуємо байти
+    Console.WriteLine($"Отримали повідомлення: {text} від {result.RemoteEndPoint.Address}:{result.RemoteEndPoint.Port}");
+}
+
+
+
